60c01fdee8eb4715b036c4ca07be925f53ea76a7
Mehran Prs <mehr.prs@gmail.com>
2020-10-06 21:57:09 +0330

    Modify the just_master_tags script

--- just_master_tags.sh
+++ just_master_tags.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
 # Check [this link](https://travis-ci.community/t/only-build-tags-that-are-on-the-master-branch/10081)
 
-if [[ -n $TRAVIS_TAG ]]; then
-  # Travis does a shallow clone by default
-  # so `master` is not present in the local metadata
-  # in a build for another branch
-  git fetch origin master:master
-  if ! (git branch --contains "$TRAVIS_TAG" | grep -qxE '. master'); then
+# if travis_tag is not set or is empty
+if [[ -z $TRAVIS_TAG ]]; then
+ travis_terminate 0    # quit the build
+fi
+# Travis does a shallow clone by default
+# so `master` is not present in the local metadata
+# in a build for another branch
+git fetch origin master:master
+if ! (git branch --contains "$TRAVIS_TAG" | grep -qxE '. master'); then
     travis_terminate 0    # quit the build early
-  fi
 fi
